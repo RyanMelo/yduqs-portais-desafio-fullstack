d2135d6af7d36c9ced34da3495afe0d822357de9
RyanMelo <ryangarciamelo@gmail.com>
2025-09-20 12:20:08 -0300

    feat(backend): create enrollment module
    This commit aims to create an enrollment module with controller, service, and repository layers and configure the initial test files.

---
+++ backend/src/enrollment/enrollment.controller.spec.ts
@@ -0,0 +1,16 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EnrollmentController } from './enrollment.controller';
+import { EnrollmentService } from './enrollment.service';
+
+describe('EnrollmentController', () => {
+  let appController: EnrollmentController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [EnrollmentController],
+      providers: [EnrollmentService],
+    }).compile();
+
+    appController = app.get<EnrollmentController>(EnrollmentController);
+  });
+});
