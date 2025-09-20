2619c1357bd9502a2c3ca678b1032ab312e7e900
RyanMelo <ryangarciamelo@gmail.com>
2025-09-20 14:01:56 -0300

    fix(backend): import modules EnrollmentRepository and PrismaService

--- backend/src/enrollment/enrollment.module.ts
+++ backend/src/enrollment/enrollment.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EnrollmentController } from './enrollment.controller';
 import { EnrollmentService } from './enrollment.service';
+import { EnrollmentRepository } from './enrollment.repository';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   imports: [],
   controllers: [EnrollmentController],
-  providers: [EnrollmentService],
+  providers: [EnrollmentService, EnrollmentRepository, PrismaService],
 })
 export class EnrollmentModule {}
