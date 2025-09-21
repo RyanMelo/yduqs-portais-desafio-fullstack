b46597b9d84f29a6ca1087639bb9c6d68e8b1ab3
RyanMelo <ryangarciamelo@gmail.com>
2025-09-21 20:10:07 -0300

    test(backend): add unit tests for saveEnrollment method in enrollment service
    Tests Cases:
    - should be create enrollment with success
    - should be return conflict

--- backend/src/enrollment/enrollment.service.spec.ts
+++ backend/src/enrollment/enrollment.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EnrollmentService } from './enrollment.service';
+import { CreateEnrollmentDto } from './create-enrollment.dto';
 import { EnrollmentRepository } from './enrollment.repository';
+import { ConflictException } from '@nestjs/common';
 
 const mockEnrollmentRepository = {
   createEnrollment: jest.fn(),
@@ -28,5 +30,60 @@ describe('EnrollmentService', () => {
 
   it('should be defined', () => {
     expect(enrollmentService).toBeDefined();
+  });
+
+  it('should be create enrollment with success', async () => {
+    const enrollmentMock: CreateEnrollmentDto = {
+      courseType: 'DISTANCE',
+      name: 'Maria José',
+      documentNumber: '45685887689',
+      birthdate: '1995-10-20',
+      email: 'mariajose@gmail.com',
+      phone: '21999998888',
+      highSchoolGraduation: 2013,
+      terms: true,
+      whatsAppNotifications: true,
+    };
+
+    mockEnrollmentRepository.createEnrollment.mockResolvedValue({
+      id: 'df5fabc3-48e3-45c9-ad80-b9bcfafb44c2',
+      ...enrollmentMock,
+    });
+
+    const result = await enrollmentService.saveEnrollment(enrollmentMock);
+
+    expect(result).toBeDefined();
+  });
+
+  it('should be return conflict', async () => {
+    const enrollmentMock: CreateEnrollmentDto = {
+      courseType: 'DISTANCE',
+      name: 'Maria José',
+      documentNumber: '45685887689',
+      birthdate: '1995-10-20',
+      email: 'mariajose@gmail.com',
+      phone: '21999998888',
+      highSchoolGraduation: 2013,
+      terms: true,
+      whatsAppNotifications: true,
+    };
+
+    mockEnrollmentRepository.checkIfEnrollmentExists.mockResolvedValue(true);
+
+    try {
+      await enrollmentService.saveEnrollment(enrollmentMock);
+
+      fail('Should have thrown a ConflictException');
+    } catch (error) {
+      expect(error).toBeInstanceOf(ConflictException);
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(error.response.error).toEqual('Error ao cadastrar matricula.');
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(error.response.message).toEqual([
+        'Já existe uma matricula cadastrado com este e-mail ou CPF.',
+      ]);
+    }
   });
 });
