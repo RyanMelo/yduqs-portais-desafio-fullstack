299ae625529ba7028d346ec8fb1fceee3adcb209
RyanMelo <ryangarciamelo@gmail.com>
2025-09-21 15:25:07 -0300

    feat(backend): add structured logs in the enrollment service

--- backend/src/enrollment/enrollment.service.ts
+++ backend/src/enrollment/enrollment.service.ts
@@ -2,12 +2,16 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { EnrollmentRepository } from './enrollment.repository';
 import { CreateEnrollmentDto } from './create-enrollment.dto';
 import { Enrollment } from '@prisma/client';
+import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class EnrollmentService {
   constructor(private readonly enrollmentRepository: EnrollmentRepository) {}
+  private readonly logger = new Logger(EnrollmentService.name);
 
   async saveEnrollment(enrollment: CreateEnrollmentDto): Promise<Enrollment> {
+    this.logger.log('Iniciando processo de salvar matricula...');
+
     const enrollmentExist =
       await this.enrollmentRepository.checkIfEnrollmentExists(
         enrollment.email,
@@ -15,6 +19,14 @@ export class EnrollmentService {
       );
 
     if (enrollmentExist) {
+      this.logger.warn(
+        'Ja existe uma matricula cadastrado com e-mail ou CPF.',
+        {
+          email: enrollment.email,
+          documentNumber: enrollment.documentNumber,
+        },
+      );
+
       throw new ConflictException(
         ['Já existe uma matricula cadastrado com este e-mail ou CPF.'],
         {
@@ -24,9 +36,15 @@ export class EnrollmentService {
       );
     }
 
-    return this.enrollmentRepository.createEnrollment({
+    const newEnrollment = await this.enrollmentRepository.createEnrollment({
       ...enrollment,
       birthdate: new Date(enrollment.birthdate),
     });
+
+    this.logger.log('Matricula salva com sucesso.', {
+      id: newEnrollment.id,
+    });
+
+    return newEnrollment;
   }
 }
