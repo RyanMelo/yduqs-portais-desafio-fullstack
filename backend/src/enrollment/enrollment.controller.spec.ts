a795769b11bab730d5e2650cc7791f278ddf8478
RyanMelo <ryangarciamelo@gmail.com>
2025-09-21 22:16:23 -0300

    test(backend): implement unit test for createEnrollment method in enrollment controller
    Teste Cases:
    - /enrollment (POST) should be create new enrollment

--- backend/src/enrollment/enrollment.controller.spec.ts
+++ backend/src/enrollment/enrollment.controller.spec.ts
@@ -1,6 +1,7 @@
 import { EnrollmentService } from './enrollment.service';
 import { Test } from '@nestjs/testing';
 import { EnrollmentController } from './enrollment.controller';
+import { CreateEnrollmentDto } from './create-enrollment.dto';
 import { EnrollmentRepository } from './enrollment.repository';
 
 describe('EnrollmentController', () => {
@@ -18,5 +19,44 @@ describe('EnrollmentController', () => {
 
     enrollmentController = moduleRef.get(EnrollmentController);
     enrollmentService = moduleRef.get(EnrollmentService);
+  });
+
+  it('/enrollment (POST) should be create new enrollment', async () => {
+    const enrollmentMock: CreateEnrollmentDto = {
+      name: 'Maria José',
+      documentNumber: '45685887689',
+      birthdate: '1995-10-20',
+      email: 'mariajose@gmail.com',
+      phone: '21999998888',
+      highSchoolGraduation: 2013,
+      terms: true,
+      courseType: 'INPERSON',
+      totalValue: 3059.11,
+      numberOfInstallments: 12,
+      whatsAppNotifications: true,
+    };
+
+    const resultMock = {
+      id: 'df5fabc3-48e3-45c9-ad80-b9bcfafb44c2',
+      name: 'Maria José',
+      documentNumber: '45685887689',
+      birthdate: new Date('1995-10-20'),
+      email: 'mariajose@gmail.com',
+      phone: '21999998888',
+      highSchoolGraduation: 2013,
+      terms: true,
+      courseType: 'INPERSON' as const,
+      totalValue: 3059.11,
+      numberOfInstallments: 12,
+      whatsAppNotifications: true,
+    };
+
+    jest
+      .spyOn(enrollmentService, 'saveEnrollment')
+      .mockImplementation(() => Promise.resolve(resultMock));
+
+    expect(await enrollmentController.createEnrollment(enrollmentMock)).toBe(
+      resultMock,
+    );
   });
 });
