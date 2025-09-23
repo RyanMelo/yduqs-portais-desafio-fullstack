df767f9fb99ef92439f9fd0e8f4c9f7d43c0724f
RyanMelo <ryangarciamelo@gmail.com>
2025-09-22 22:38:03 -0300

    fix(frontend): adjust MainHeader padding for responsive design

--- frontend/src/components/MainHeader.tsx
+++ frontend/src/components/MainHeader.tsx
@@ -1,17 +1,23 @@
-import Image from 'next/image'
+'use client'
 
+import Image from 'next/image'
 import { Box } from "@mui/material";
 
 export default function MainHeader() {
   return (
-    <Box sx={{
+    <Box sx={(theme) => ({
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'start',
       padding: '16px 88px',
 
       backgroundColor: 'background.default',
-    }}>
+
+      [theme.breakpoints.down('md')]: {
+        padding: '16px 16px',
+      }
+
+    })}>
       <Image
         src="/estacio-logo.svg"
         width={158}
