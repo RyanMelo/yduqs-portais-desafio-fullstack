31c2e1d66ff91035d23275c03590def7aba562c7
RyanMelo <ryangarciamelo@gmail.com>
2025-09-22 21:54:09 -0300

    chore(frontend): configure theme

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -3,8 +3,23 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   typography: {
-    fontFamily: 'var(--font-roboto)',
+    fontFamily: 'var(--font-inter)',
   },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontFamily: 'var(--font-inter)',
+          fontSize: '16px',
+          textTransform: 'none',
+          fontWeight: 500,
+          borderRadius: 8,
+          boxShadow: 'none',
+          padding: '16px 24px',
+        }
+      }
+    }
+  }
 });
 
 export default theme;
