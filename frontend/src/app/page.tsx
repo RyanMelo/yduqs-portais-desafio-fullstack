b1f2fc3601eb005f3e02b9ced75cb1632abf518c
RyanMelo <ryangarciamelo@gmail.com>
2025-09-24 01:19:41 -0300

    refactor(frontend): improve homepage layout and add Footer component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,28 @@
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import SecodaryHeader from "@/components/ui/SecodaryHeader";
 import CardsSection from "@/components/CardsSection";
+import Footer from "@/components/ui/Footer";
 
 export default function Home() {
   return (
-    <>
-      <SecodaryHeader
-        title="Vamos começar, escolha as opções do seu curso"
-        subtitle="Use os filtros para saber o preço do seu curso e fazer sua inscrição."
-      />
+    <Box
+      sx={{
+        minHeight: 'calc(100vh - 80px)',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <main style={{ flex: 1 }}>
+        <SecodaryHeader
+          title="Vamos começar, escolha as opções do seu curso"
+          subtitle="Use os filtros para saber o preço do seu curso e fazer sua inscrição."
+        />
+        <Container>
+          <CardsSection/>
+        </Container>
+      </main>
 
-      <Container>
-        <CardsSection/>
-      </Container>
-    </>
+      <Footer/>
+    </Box>
   );
 }
