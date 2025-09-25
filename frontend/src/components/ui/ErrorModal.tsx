5481b68deaf6de43505a55fe29e96fdd1e225986
Ryan Melo <ryangarciamelo@gmail.com>
2025-09-25 10:13:58 -0300

    refactor(frontend): use alert in ErrorModal.tsx to visualizer error

--- frontend/src/components/ui/ErrorModal.tsx
+++ frontend/src/components/ui/ErrorModal.tsx
@@ -4,10 +4,11 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
-  DialogContentText,
   DialogActions,
   Button,
   ThemeProvider,
+  Alert,
+  Typography
 } from '@mui/material';
 import theme from '@/theme';
 
@@ -18,23 +19,41 @@ interface ErrorModalProps {
   message: string | string[];
 }
 
-export default function ErrorModal({ open, onClose, title, message }: ErrorModalProps) {
+export default function ErrorModal({open, onClose, title, message}: ErrorModalProps) {
   return (
     <ThemeProvider theme={theme}>
       <Dialog open={open} onClose={onClose}>
         <DialogTitle>{title}</DialogTitle>
         <DialogContent>
-          {Array.isArray(message) ? (
-            message.map((msg, index) => (
-              <DialogContentText key={index}>{msg}</DialogContentText>
-            ))
-          ) : (
-            <DialogContentText>{message}</DialogContentText>
-          )}
+          <Alert severity="warning">
+            {Array.isArray(message) ? (
+              message.map((msg, index) => (
+                <Typography key={index}>{msg}</Typography>
+              ))
+            ) : (
+              <Typography>{message}</Typography>
+            )}
+          </Alert>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={onClose} color="primary" autoFocus>
-            Fechar
+
+        <DialogActions
+          sx={(theme) => ({
+            justifyContent: 'right',
+            paddingRight: '24px',
+            paddingLeft: '24px',
+
+            [theme.breakpoints.down('md')]: {
+              justifyContent: 'center',
+            }
+          })}
+        >
+          <Button
+            onClick={onClose}
+            color="primary"
+            variant="contained"
+            sx={{maxWidth: '200px', borderRadius: '8px'}}
+          >
+            Ok, Entendi!
           </Button>
         </DialogActions>
       </Dialog>
